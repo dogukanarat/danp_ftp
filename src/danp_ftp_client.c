4cdaa22d159787eb45c8603cffc2d1f734d57a08
dogukanarat <aratdogukan@gmail.com>
2025-12-19 23:32:01 +0300

    Add initial files

---
+++ src/danp_ftp_client.c
@@ -0,0 +1,24 @@
+/* danp_ftp_client.c - one line definition */
+
+/* All Rights Reserved */
+
+/* Includes */
+
+
+/* Imports */
+
+
+/* Definitions */
+
+
+/* Types */
+
+
+/* Forward Declarations */
+
+
+/* Variables */
+
+
+/* Functions */
+
---
+++ src/danp_ftp_server.c
@@ -0,0 +1,24 @@
+/* danp_ftp_server.c - one line definition */
+
+/* All Rights Reserved */
+
+/* Includes */
+
+
+/* Imports */
+
+
+/* Definitions */
+
+
+/* Types */
+
+
+/* Forward Declarations */
+
+
+/* Variables */
+
+
+/* Functions */
+
